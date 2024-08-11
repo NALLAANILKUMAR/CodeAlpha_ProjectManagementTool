67003dc7efc6062566c6d7f7c525895dde025613
ANIL KUMAR NALLA <nalla.anilkumar29@gmail.com>
2024-08-11 23:09:26 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use('/api/auth', authRoutes);
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  
+  socket.on('join room', (roomID) => {
+    socket.join(roomID);
+  });
+
+  socket.on('send message', (data) => {
+    io.to(data.roomID).emit('receive message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
